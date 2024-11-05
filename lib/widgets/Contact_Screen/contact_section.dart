e1a857198345b04a5757533fc49ac02ff2a9559f
Ansh Gupta <111687229+ianshgupta@users.noreply.github.com>
2024-11-05 10:07:55 +0530

    Update contact_section.dart

--- lib/widgets/Contact_Screen/contact_section.dart
+++ lib/widgets/Contact_Screen/contact_section.dart
@@ -133,7 +133,7 @@ class ContactSection extends StatelessWidget {
                     backgroundColor: CustomColor.bgLight1,
                     child: IconButton(
                         onPressed: () {
-                          _launchURL('https://www.instagram.com/anshh.gupta/');
+                          _launchURL('https://ansh-gupta.netlify.app/');
                         },
                         icon: Icon(Icons.person)),
                   ),
